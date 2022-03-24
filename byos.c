678121f0ec57f19f0f015c1cadb2170a5a094465
Anindro Bhattacharya <69637288+AB20CS@users.noreply.github.com>
2022-03-23 20:08:43 -0400

    Update byos.c

--- byos.c
+++ byos.c
@@ -9,97 +9,135 @@
 #include "byos.h"
 // Find out what other #include's you need! (E.g., see man pages.)
 
+void return_from_command(int *fd) {
+    if ((*fd) != STDOUT_FILENO) {
+        close(*fd);
+    }
+    *fd = STDOUT_FILENO; // restore original stdout
+}
+
+int echo(int fd, const struct cmd *c) {
+    write(fd, c->data.echo.arg, strlen(c->data.echo.arg));
+    
+    return_from_command(&fd);
+    return 0;
+}
+
+int forx(int fd, const struct cmd *c) {
+    pid_t pid;
+    int status, exit_status;
+    
+    int pipefd[2];
+    pipe(pipefd);
+
+    pid = fork();
+
+    if (pid == 0) { // child process
+        close(pipefd[0]); // close reading end in child
+        dup2(pipefd[1], STDOUT_FILENO); // send stdout to pipe
+
+        close(pipefd[1]);
+
+        int exec_ret;
+        if ((exec_ret = execvp(c->data.forx.pathname, c->data.forx.argv)) == -1) {
+            perror("exec");
+            fd = STDOUT_FILENO; // restore original stdout
+            exit(127);
+        }
+    }
+    else { // parent process
+        wait(&status);
+
+        close(pipefd[1]); // close writing end in parent
+        
+        char buf[1024];
+        int nbytes;
+        while ((nbytes = read(pipefd[0], buf, sizeof(buf))) > 0) {
+            write(fd, buf, nbytes);
+        }
+
+        if (WIFEXITED(status)) {
+            return_from_command(&fd);
+            return WEXITSTATUS(status);
+        }
+        else if (WIFSIGNALED(status)) {
+            return_from_command(&fd);
+            return 128 + WTERMSIG(status);
+        }
+    }
+}
+
+int list(int fd, const struct cmd *c) {
+    
+    int return_val = 0;
+    int fd_list = fd;
+    for (int i = 0; i < c->data.list.n; i++) {
+        // save stdout_redir file currently in command and change it to fd
+        bool req_redir = false;
+        if (c->data.list.cmds[i].redir_stdout == NULL &&  c->redir_stdout != NULL) {
+            c->data.list.cmds[i].redir_stdout = malloc(1024);
+            strcpy(c->data.list.cmds[i].redir_stdout, c->redir_stdout);
+            req_redir = true;
+        }
+        
+        // call command
+        // if (c->data.list.cmds[i].type == ECHO) {
+        //     return_val = echo(fd, &(c->data.list.cmds[i]));
+        // }
+        // else if (c->data.list.cmds[i].type == FORX) {
+        //     return_val = forx(fd, &(c->data.list.cmds[i]));
+        // }
+        // else if (c->data.list.cmds[i].type == LIST) {
+        //     return_val = list(fd, &(c->data.list.cmds[i]));
+        // }
+        return_val = interp(&(c->data.list.cmds[i]));
+        
+        // restore command's redirection info
+        if (req_redir) {
+            free(c->data.list.cmds[i].redir_stdout);
+            c->data.list.cmds[i].redir_stdout = NULL;
+        }
+
+        // if command or descendent is killed by SIGINT
+        if (return_val == 128 + SIGINT) {
+            return return_val;
+        }
+
+        return_from_command(&fd);
+        fd = fd_list;
+    }
+    return_from_command(&fd);
+    return return_val;
+}
+
 int interp(const struct cmd *c)
 {
     int fd;
     if (c->redir_stdout == NULL) {
-        printf("write to stdout\n");
+        // printf("write to stdout\n");
         fd = STDOUT_FILENO;
     }
     else {
         fd = creat(c->redir_stdout, 0666);
-        printf("redirect stdout to %s (%d)\n", c->redir_stdout, fd);
+        // printf("redirect stdout to %s (%d)\n", c->redir_stdout, fd);
         if (fd == -1) {
             perror("open redirection file");
-            return 1;
+            exit(1);
         }
     }
     
     // ECHO
     if (c->type == ECHO) {
-        write(fd, c->data.echo.arg, strlen(c->data.echo.arg));
-        return 0;
+        return echo(fd, c);
     } 
 
     // FORX
     else if (c->type == FORX) {
-        pid_t pid;
-        int status, exit_status;
-        
-        int pipefd[2];
-        pipe(pipefd);
-
-        pid = fork();
-
-        if (pid == 0) { // child process
-            close(pipefd[0]); // close reading end in child
-            dup2(pipefd[1], STDOUT_FILENO); // send stdout to pipe
-
-            // close(pipefd[1]);
-
-            int exec_ret;
-            if ((exec_ret = execvp(c->data.forx.pathname, c->data.forx.argv)) == -1) {
-                perror("exec");
-                exit(127);
-            }
-        }
-        else { // parent process
-            wait(&status);
-
-            close(pipefd[1]); // close writing end in parent
-            
-            char buf[1024];
-            int nbytes;
-            while ((nbytes = read(pipefd[0], buf, sizeof(buf))) > 0) {
-                printf("%d\n", fd);
-                write(fd, buf, nbytes);
-            }
-
-            if (WIFEXITED(status)) {
-                return WEXITSTATUS(status);
-            }
-            else if (WIFSIGNALED(status)) {
-                return 128 + WTERMSIG(status);
-            }
-        }
+        return forx(fd, c);
     }
 
     // LIST
     else if (c->type == LIST) {
-        int return_val = 0;
-        for (int i = 0; i < c->data.list.n; i++) {
-            // save stdout_redir file currently in command and change it to fd
-            bool req_redir = false;
-            if (c->data.list.cmds[i].redir_stdout == NULL &&  c->redir_stdout != NULL) {
-                c->data.list.cmds[i].redir_stdout = malloc(1024);
-                strcpy(c->data.list.cmds[i].redir_stdout, c->redir_stdout);
-                req_redir = true;
-            }
-            
-            // call command
-            return_val = interp(&(c->data.list.cmds[i]));
-            
-            // restore command's redirection info
-            if (req_redir) {
-                free(c->data.list.cmds[i].redir_stdout);
-                c->data.list.cmds[i].redir_stdout = NULL;
-            }
-
-            // if command or descendent is killed by SIGINT
-            if (return_val == 128 + SIGINT) {
-                return return_val;
-            }
-        }
-        return return_val;
+        return list(fd, c);
     }
 }
